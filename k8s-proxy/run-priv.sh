5ddd561c5d06995995efc2ec5e825530b97d7a0d
ayoub mrini <amrini@wiremind.fr>
2020-01-21 10:26:27 +0100

    Disable Telepresence Proxy logs

--- k8s-proxy/run-priv.sh
+++ k8s-proxy/run-priv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 /usr/sbin/sshd -e
-exec env PYTHONPATH=/usr/src/app twistd --pidfile= -n -y ./forwarder.py
+exec env PYTHONPATH=/usr/src/app twistd -l=- --pidfile= -n -y ./forwarder.py 2>&1 >/dev/null
